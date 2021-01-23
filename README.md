51f69b2ee6bcb2e74d26aa204395b09aa240ca9e
alandev2 <alanworking1@gmail.com>
2021-01-22 22:37:46 -0300

    organize readme to project

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # API REST FULL SALES SYSTEM 😁
 
-> Writing details... wait! :)
+### I'm still projecting, wait, wait! 💙
+
+## Project Resume:
+> ideas/stacks/modules/tools/concepts
+- Typescript [ language ]
+- TypeORM / Docker / PostGres [ db service ]
+- Great structure for scalable projects [ concepts/ideia ]
+- Eslint / Prettier / EditorConfig [ code service ]
+
+## Concepts for you to implement!
+> Everything i wrote bellow was implemented in this repository.
+
+- class AppError and handle errors of the improved format
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -12,6 +13,23 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+//midleware error
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () =>
   console.log(
