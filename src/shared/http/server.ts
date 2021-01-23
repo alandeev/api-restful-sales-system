48d7a86f9db244ff0f04eedd7e85d614ceeb8d2c
alandev2 <alanworking1@gmail.com>
2021-01-22 22:20:56 -0300

    creating AppError

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,21 @@
-console.log('Hello dev!');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import routes from './routes';
+
+const app = express();
+
+//settings
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () =>
+  console.log(
+    process.env.SERVER_PORT,
+    `Server running in the port ${process.env.SERVER_PORT}`,
+  ),
+);
