59e13ae6e5b536500fa2f901d6f7a2faa409df52
alandev2 <alanworking1@gmail.com>
2021-01-23 22:54:26 -0300

    created all service of products

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,16 @@
+import { getCustomRepository } from 'typeorm';
+
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+import Product from '../typeorm/entities/Product';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const products = productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
